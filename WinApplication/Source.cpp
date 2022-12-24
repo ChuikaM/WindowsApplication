0a3aea5cb2127ff306f6fbe38a27136439181e95
matveidoroshenko@gmail.com <matveidoroshenko@gmail.com>
2022-12-24 18:48:27 +0300

    Removed std::cout and std::cin

--- WinApplication/Source.cpp
+++ WinApplication/Source.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
 int main() {
-	std::cout << "Hello, world\n";
-	int i;
-	std::cin >> i;
-	std::cout << i;
-	std::cout << "Hi";
+	int a,b;
+	std::cin >> a >> b;
+	if (a > b)std::cout << "A greater then B";
+	else std::cout << "B greater then A";
 }
